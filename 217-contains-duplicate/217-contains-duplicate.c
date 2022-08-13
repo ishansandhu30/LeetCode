34fb064764305db4782df9574b8b4e1ba394f134
ishansandhu30 <50429696+ishansandhu30@users.noreply.github.com>
2022-08-13 15:59:47 -0700

    Time: 132 ms (71.39%), Space: 12.8 MB (49.71%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.c
+++ 217-contains-duplicate/217-contains-duplicate.c
@@ -1,20 +1,3 @@
-/*
-int cmp(const void *a, const void *b)
-{
-    return (*(int*)a - *(int*)b);
-}
-
-bool containsDuplicate(int* nums, int numsSize)
-{
-    qsort(nums, numsSize, sizeof(int), cmp); 
-
-    for (int i = 1; i < numsSize; i++)
-        if (nums[i] == nums[i-1])
-            return true;
-
-    return false;
-}
-*/
 bool containsDuplicate(int* nums, int numsSize){
 int cmp(const void *a, const void *b)
 {

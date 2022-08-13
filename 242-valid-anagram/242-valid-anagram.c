99eb243d390dfb284ff5d4b4337eab4bf93b8916
ishansandhu30 <50429696+ishansandhu30@users.noreply.github.com>
2022-08-13 16:11:13 -0700

    Time: 29 ms (27.73%), Space: 6.1 MB (26.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.c
+++ 242-valid-anagram/242-valid-anagram.c
@@ -5,7 +5,7 @@ bool isAnagram(char * s, char * t){
     qsort(s, (size_t) strlen(s), (size_t) sizeof(char), cmpfunc);
     qsort(t, (size_t) strlen(t), (size_t) sizeof(char), cmpfunc);
     int result = strcmp(t, s);
-    printf("%d", result);
+  
     if(result == 0){
         return true;
     }else{

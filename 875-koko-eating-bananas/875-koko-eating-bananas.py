56a45dcbbb318393ba1b129c982860b98d9a50ad
ishansandhu30 <50429696+ishansandhu30@users.noreply.github.com>
2022-08-31 17:20:19 -0700

    Time: 550 ms (77.03%), Space: 15.5 MB (24.06%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,16 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left = ceil(sum(piles) / h) # lower bound of Binary Search 
-        right = max(piles) # upper bound of Binary Search 
+        left = ceil(sum(piles) / h)  
+        right = max(piles)  
         while left < right:
-            mid = (left + right) // 2 # we check for k=mid
+            mid = (left + right)//2
             total_time = 0
             for i in piles:
                 total_time += ceil(i / mid)
                 if total_time > h:
                     break
             if total_time <= h:
-                right = mid # answer must lie to the left half (inclusive of current value ie mid)
+                right = mid 
             else:
-                left = mid + 1 # answer must lie to the right half (exclusive of current value ie mid)
+                left = mid + 1 
         return right

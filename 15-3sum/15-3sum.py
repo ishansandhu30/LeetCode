23fc29f7eb9c39969fbefc415060c9b589ac6f49
ishansandhu30 <50429696+ishansandhu30@users.noreply.github.com>
2022-09-09 18:07:27 -0700

    Time: 1282 ms (56.21%), Space: 18.1 MB (71.54%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,7 +3,8 @@ class Solution:
         arr = []
         nums = sorted(nums)
         
-       
+        
+   
        
         for i in range(len(nums)):
             if i>0 and nums[i]==nums[i-1]:

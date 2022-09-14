00796f566a64b9f9c4c54da94201ca34b5ea5245
ishansandhu30 <50429696+ishansandhu30@users.noreply.github.com>
2022-09-13 18:03:43 -0700

    Time: 654 ms (5.02%), Space: 14.3 MB (96.05%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,17 +1,36 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        for c in tokens:
-            if c == "+":
-                stack.append(stack.pop() + stack.pop())
-            elif c == "-":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b - a)
-            elif c == "*":
-                stack.append(stack.pop() * stack.pop())
-            elif c == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b / a))
-            else:
-                stack.append(int(c))
-        return stack[0]
+        val = 0
+        if len(tokens) <= 1:
+            return int(tokens[0])
+        for i in tokens:
+            stack.append(i)
+            if i == "+":
+                index = stack.index(i)
+                val = int(stack[index-2])+int(stack[index-1])
+                stack[index-2] = val
+                stack.pop()
+                stack.pop()
+                
+            elif i == "-":
+                index = stack.index(i)
+                val = int(stack[index-2])-int(stack[index-1])
+                stack[index-2] = val
+                stack.pop()
+                stack.pop()
+            elif i == "*":
+                index = stack.index(i)
+                val = int(stack[index-2])*int(stack[index-1])
+                stack[index-2] = val
+                stack.pop()
+                stack.pop()
+            elif i == "/":
+                index = stack.index(i)
+                val = int(int(stack[index-2])/int(stack[index-1]))
+                stack[index-2] = val
+                stack.pop()
+                stack.pop()
+        return val
+                
+                
